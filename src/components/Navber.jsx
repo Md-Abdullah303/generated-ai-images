daa9b52c64c2be48989257c2a5ae6ccb0ef7ddd2
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-04-30 15:33:31 +0600

    added proxy in profile and all-photos/details page

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -36,9 +36,6 @@ const Navbar = () => {
             <Link href={"/all-photos"}>All Photos</Link>
           </li>
           <li>
-            <Link href={"/pricing"}>Pricing</Link>
-          </li>
-          <li>
             <Link href={"/profile"}>Profile</Link>
           </li>
         </ul>
